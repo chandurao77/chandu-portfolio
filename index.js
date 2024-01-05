97317f935f7bec9401b6246a7723f0b9b29b3ebe
chandurao77 <98127784+chandurao77@users.noreply.github.com>
2024-01-05 18:59:50 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function toggleAboutMe() {
+    var aboutMe = document.getElementById("aboutMe");
+    if (aboutMe.style.display === "none") {
+        aboutMe.style.display = "block";
+    } else {
+        aboutMe.style.display = "none";
+    }
+}
